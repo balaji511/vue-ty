efd8208cd556739030414d4f0f6e4f8d4441c623
Balaji Pavan Kumar <101182094+balaji511@users.noreply.github.com>
2023-01-26 10:47:41 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-btn class="primary">Button</v-btn>
+  </v-app>
+</template>
+
+<script>
+import Boxer from "./components/Boxer";
+// import Playground from "./components/Playground";
+
+export default {
+  name: "App",
+  components: {
+    Boxer,
+    // Playground
+  },
+};
+</script>
+
---
+++ src/components/Boxer.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-icon>mdi-vuetify</v-icon>
+</template>
+
+<script>
+export default {
+  name: "Boxer",
+  data() {
+    return {};
+  },
+};
+</script>
